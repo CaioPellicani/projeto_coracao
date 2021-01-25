828545e7b8dca704e071bf329532fe28e8ebe313
CaioPellicani <caiopellicani@hotmail.com>
2021-01-25 20:06:49 -0300

    mv executavel, interface virou biblioteca

--- interface/main.cpp
+++ interface/main.cpp
@@ -1,12 +1,12 @@
-#include "frmprincipal.h"
-#include <t2.h>
+/*#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    t2();
-    QApplication a(argc, argv);
-    frmPrincipal w;
+    QApplication app(argc, argv);
+
+    MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
+*/
